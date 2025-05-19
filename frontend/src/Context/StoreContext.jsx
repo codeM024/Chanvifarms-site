ec5bf0440f668898eb9b8875f694395ed09dbce3
codeM024 <manisake28@gmail.com>
2025-05-20 01:26:15 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://chanvifarms-site-backend.onrender.com"
     const [token, setToken] = useState(() => localStorage.getItem('token') || "")
     const [food_list, setFoodList] = useState([]);
     const [loading, setLoading] = useState(true);
