25519271dfa903d97bdb695016922a7adfab7309
codeM024 <manisake28@gmail.com>
2025-08-19 12:32:42 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import authMiddleware from "../middleware/auth.js"
-import { listOrders, placeOrder, updateStatus, userOrders, verifyOrderPayment, confirmWhatsAppPayment } from "../controllers/orderController.js"
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrderPayment, confirmWhatsAppPayment, deleteOrder } from "../controllers/orderController.js"
 
 const orderRouter = express.Router();
 
@@ -10,5 +10,6 @@ orderRouter.post("/userorders", authMiddleware, userOrders);
 orderRouter.get('/list', listOrders);
 orderRouter.post('/status', updateStatus);
 orderRouter.post('/confirm-whatsapp-payment', authMiddleware, confirmWhatsAppPayment);
+orderRouter.delete('/delete/:id', deleteOrder);
 
 export default orderRouter;
