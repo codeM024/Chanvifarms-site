3489279512b35d8cf96bd4e93769ef2b7c4f222e
codeM024 <manisake28@gmail.com>
2025-08-21 17:24:43 +0530

    Update Orders.jsx

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -14,7 +14,7 @@ import {
 } from "react-icons/fa";
 
 const Orders = () => {
-  const url = "http://localhost:4000";
+  const url = "https://chanvifarms-site-backend.onrender.com";
   const [orders, setOrders] = useState([]);
   const [copiedOrderId, setCopiedOrderId] = useState(null);
   const [startDate, setStartDate] = useState("");
